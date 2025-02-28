3da33906c06264287f1a67bae884718f6c58a368
Pxdro-410 <pedrojuliocaso@gmail.com>
2025-02-27 23:09:19 -0600

    Ejemplos de tipos de datos y estructuras

---
+++ TipoDatos.jl
@@ -0,0 +1,5 @@
+# Definimos una variale y se le asigna una cantidad de bits y sin signo (solo para este caso) y se le asigna un valor
+num::UInt32 = 410
+
+# Se muestra su valor y tipo de dato
+println("Número: ", num, " \nTipo de dato: ", typeof(num))
---
+++ TipoEstruc.jl
@@ -0,0 +1,26 @@
+# Se define un Tuple con distintos datos (string, int y booleano en este caso)
+carro = ("Ferrari", 90, true)
+
+# Con el indice se accede a los elementos
+println("Primer elemento: ", carro[1]) #en este caso puede ser la marca del carro
+println("Segundo elemento: ", carro[2]) # en este caso puede ser el modelo del carro
+println("tercer elemento: ", carro[3]) # en este caso puede ser si es modificado o no el carro
+
+
+
+# Se define un string se puede declarar antes o no con el operador ::
+nuevostring = "Hola Mundo!"
+
+# se imprime el string
+println("El contenido es: ", nuevostring)
+
+
+
+# Se define un rango del 1 al 10
+nuevorango = 1:10  
+
+# Se imprime el rango
+println("Rango: ", nuevorango)
+
+# un ejemplo de como acceder a los elementos
+println("Primer elemento: ", nuevorango[1])
